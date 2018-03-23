3999e71a5595415ef123c81d55a2ea67d6cab9ec
Drorush <drorraba@gmail.com>
2018-03-23 22:09:03 +0300

    finished part 2

--- B18_Ex01_04/Program.cs
+++ B18_Ex01_04/Program.cs
@@ -10,7 +10,102 @@ namespace B18_Ex01_04
     {
         public static void Main()
         {
-            // this is an entry point
+            int num;
+
+            string input = GetInput();
+            CheckIfPalindrome(input);
+            if (int.TryParse(input, out num))
+            {
+                IsEven(num);
+            }
+            else
+            {
+                LowerCaseCounter(input);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void LowerCaseCounter(string i_input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < i_input.Length; i++)
+            {
+                if (char.IsLower(i_input[i]))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("The string contains " + count + " lower case letters");
+        }
+
+        private static void IsEven(int i_num)
+        {
+            if (i_num % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+
+        private static void CheckIfPalindrome(string i_input)
+        {
+            if (IsPalindrome(i_input))
+            { 
+                Console.WriteLine("the string is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("the string isn't palindrome");
+            }
+        }
+
+        private static Boolean IsPalindrome(string i_input)
+        {
+            bool isPalindrome = true;
+            int startIndex = 0;
+            int endIndex = i_input.Length - 1;
+
+            while (isPalindrome && (startIndex <= endIndex))
+            {
+                isPalindrome = i_input[startIndex] == i_input[endIndex];
+                startIndex++;
+                endIndex--;
+            }
+
+            return isPalindrome;
+        }
+
+        private static string GetInput()
+        {
+            bool isLegal = false;
+            string input = string.Empty;
+            Console.WriteLine("Please enter a string of length 8 (contains only letters or numbers) and then press enter");
+            while (!isLegal)
+            {
+                input = Console.ReadLine();
+                isLegal = IsLegal(input);
+                if (!isLegal)
+                {
+                    Console.WriteLine("Your input is illegal, please try again");
+                }
+            }
+
+            return input;
+        }
+
+        private static Boolean IsLegal(string i_input)
+        {
+            bool isLegalLength = i_input.Length == 8;
+            bool isNumeric = int.TryParse(i_input, out int n);
+            bool isAlphaBet = i_input.All(char.IsLetter);
+
+            return isLegalLength && (isNumeric || isAlphaBet);
         }
     }
 }
--- B18_Ex01_05/Program.cs
+++ B18_Ex01_05/Program.cs
@@ -10,7 +10,111 @@ namespace B18_Ex01_05
     {
         public static void Main()
         {
-            // bla bla
+            int input = GetPositiveIntegerFromUser();
+            GetMaxAndMinDigits(input);
+            CountEvens(input);
+            CountDigitsLowerThanOnesDigit(input);
+            Console.ReadLine();
+        }
+
+        private static void CountDigitsLowerThanOnesDigit(int i_num)
+        {
+            int count = 0;
+            int onesDigit = i_num % 10;
+            i_num /= 10;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i_num % 10 < onesDigit)
+                {
+                    count++;
+                }
+
+                i_num /= 10;
+            }
+
+            Console.WriteLine(count + " Of the numbers are lower than the ones digit");
+        }
+
+        private static int GetMin(int i_num)
+        {
+            int minDigit = int.MaxValue;
+            int singleDigit = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                singleDigit = i_num % 10;
+
+                if (singleDigit % 10 < minDigit)
+                {
+                    minDigit = singleDigit;
+                }
+
+                i_num /= 10;
+            }
+
+            return minDigit;
+        }
+
+        private static void CountEvens(int i_num)
+        {
+            int count = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if ((i_num % 10) % 2 == 0)
+                {
+                    count++;
+                }
+
+                i_num /= 10;
+            }
+
+            Console.WriteLine(count + " Of the digits are even");
+        }
+
+        private static void GetMaxAndMinDigits(int i_num)
+        {
+            int maxDigit = int.MinValue;
+            int originalNum = i_num;
+            int minDigit;
+            int singleDigit = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                singleDigit = i_num % 10;
+                if (singleDigit > maxDigit)
+                {
+                    maxDigit = singleDigit;
+                }
+
+                i_num /= 10;
+            }
+
+            minDigit = GetMin(originalNum);
+            Console.WriteLine("The maximum digit is " + maxDigit);
+            Console.WriteLine("The minimum digit is " + minDigit);
+        }
+
+        private static int GetPositiveIntegerFromUser()
+        {
+            bool isLegal = false;
+            string input;
+            int num = 0;
+
+            Console.WriteLine("please enter 6 digit positive integer, and then press enter");
+            while (!isLegal)
+            {
+                input = Console.ReadLine();
+                isLegal = int.TryParse(input, out num) && num > 0;
+                if (!isLegal)
+                {
+                    Console.WriteLine("Your input is illegal, please try again");
+                }
+            }
+
+            Console.WriteLine("The number is: " + num);
+            return num;
         }
     }
 }
